8683c8ddf09e397dccd92d908c086ba7440f7e83
nvnhat04 <nvnhat04324@gmail.com>
2024-12-24 22:14:03 +0700

    Update db.connect.js

--- src/config/db.connect.js
+++ src/config/db.connect.js
@@ -1,3 +1,21 @@
+// import pg from "pg";
+// import dotenv from "dotenv";
+// dotenv.config();
+// const { Pool } = pg;
+
+// const pool = new Pool({
+//   host: process.env.POSTGRES_HOST,
+//   user: process.env.POSTGRES_USER,
+//   password: process.env.POSTGRES_PASSWORD,
+//   database: process.env.POSTGRES_DB,
+//   port: process.env.POSTGRES_PORT,
+//   idleTimeoutMillis: 30000,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+//   debug: false,
+// });
+
 import mysql from 'mysql2/promise';
 import dotenv from "dotenv";
 dotenv.config();
@@ -5,15 +23,27 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
+  port: process.env.MYSQL_PORT,
   database: process.env.MYSQL_DB,
   password: process.env.MYSQL_PASSWORD,
   waitForConnections: true,
   connectionLimit: 10,
-  maxIdle: 10, 
-  idleTimeout: 60000, 
+  maxIdle: 10,
+  idleTimeout: 60000,
   queueLimit: 0,
   enableKeepAlive: true,
   keepAliveInitialDelay: 0,
 });
+
+(async () => {
+  try {
+    // Test the connection
+    const connection = await pool.getConnection();
+    console.log("Connected to the database successfully!");
+    connection.release(); // Release the connection back to the pool
+  } catch (error) {
+    console.error("Failed to connect to the database:", error.message);
+  }
+})();
 
 export default pool;
